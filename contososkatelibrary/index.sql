1bfa262ee27798fd5c56c2d3739e2a56bea705f5
mufisiru002 <112650296+mufisiru002@users.noreply.github.com>
2023-01-28 20:40:32 +0100

    Add files via upload

---
+++ contososkatelibrary/index.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE if not exists `ContosoSkateLibrary`;
+show databases;
+use `ContosoSkateLibrary`;
+/*
+ run the last code at the bottom victor then go and run the other
+ files the after running the other files feel free to remove the command
+ The command is set session foreign_key_ckecks = 0; 
+*/ 
+set session foreign_key_ckecks = 0; 
